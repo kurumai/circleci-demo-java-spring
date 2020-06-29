166cf386ce27c7f0fd9b4256cfb4dacdd5dd5465
Noboru Kurumai <noboru@circleci.com>
2020-06-29 09:42:00 +0900

    leak test

--- src/main/java/com/circleci/demojavaspring/model/Quote.java
+++ src/main/java/com/circleci/demojavaspring/model/Quote.java
@@ -1,5 +1,6 @@
 package com.circleci.demojavaspring.model;
 
+import java.util.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,13 +9,29 @@ import javax.persistence.Id;
 @Entity
 public class Quote {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String quote;
 
+    private List<byte[]> leak_buffers;
+
+    public Quote() {
+        for(int i=0; i<10000; i++){
+            leak_buffers.add(new byte[1024 * 1024 * 1024]);
+        }
+    }
+    
     public Integer getId() {
         return id;
+    }
+
+    public List<byte[]> getLeak_buffers() {
+        return leak_buffers;
+    }
+
+    public void setLeak_buffers(List<byte[]> leak_buffers) {
+        this.leak_buffers = leak_buffers;
     }
 
     public String getQuote() {
@@ -24,4 +41,5 @@ public class Quote {
     public void setQuote(String quote) {
         this.quote = quote;
     }
+
 }
